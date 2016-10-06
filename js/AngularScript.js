d6bb563d7199552ff141fee775ab02f3a4d7aa64
P Rajabimehr <p.rajabi.m@gmail.com>
2016-10-06 14:08:06 -0700

    Add some comments to find what's  what

--- js/AngularScript.js
+++ js/AngularScript.js
@@ -3,19 +3,31 @@
 
     var MainController = function ($scope, $http, $interval, $log) {
 
+        //******************************************
+        //when loaded data
+
         var onUserComplete = function (response) {
             $scope.user = response.data;
             $http.get($scope.user.repos_url)
                  .then(onRepos, onError);
         }
 
+        //******************************************
+        //Repository data
+
         var onRepos = function (response) {
             $scope.repos = response.data;
         }
 
+        //******************************************
+        //error when it can't fetch the user
+
         var onError = function (reason) {
             $scope.error = "Could not fetch the user."
         };
+
+        //******************************************
+        //Coundown to 5 and search if user didn't search
 
         var decrementCountDown = function () {
             $scope.CountDown -= 1;
@@ -23,17 +35,27 @@
                 $scope.search($scope.username);
             }
         };
-        
+
+        //******************************************
+        //after 1s call startCountDown function
+
         var countDownInterval = null;
 
         var startCountDown = function () {
            countDownInterval= $interval(decrementCountDown, 1000, $scope.CountDown);
         }
 
+        //******************************************
+        //Search function for username
+
         $scope.search = function (username) {
             $log.info("Searching for " + username);
             $http.get("http://api.github.com/users/" +username)
              .then(onUserComplete, onError);
+
+            //*******************************************
+            //Stop counting down if user searched something
+
             if (countDownInterval){
                 $interval.cancel(countDownInterval);
                 $scope.CountDown = null;
