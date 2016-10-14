0b2978b32ac9ad1a15c76cd571077877b1aa5287
P Rajabimehr <p.rajabi.m@gmail.com>
2016-10-14 15:08:21 -0700

    foud the consept of  controller, model, view

--- js/script.js
+++ js/script.js
@@ -1,36 +1,4 @@
 ﻿
-(function(){
-var createworker = function () {
-    
-    var workcount = 0;
-
-    var task1 = function () {
-        workcount += 1;
-        console.log("task1 " + workcount);
-    };
-
-    var task2 = function () {
-        workcount += 1;
-        console.log("task2 " + workcount);
-    };
-
-    return {
-        job1: task1,
-        job2: task2
-
-    };
-    }
-
-
-
-
-
-var worker = createworker();
-
-worker.job1();
-worker.job2();
-worker.job1();
-worker.job1();
-
-
-}());
+var MainController = function ($scope) {
+    $scope.message = "Hello, Angular";
+}
