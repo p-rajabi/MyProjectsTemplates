3d73cc21d34f82066e0f783790c17584f9d7cd17
P Rajabimehr <p.rajabi.m@gmail.com>
2016-10-17 11:33:32 -0700

    countdowninterval by using $interval

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
     var app = angular.module("githubView", []);
 
-    var MainController = function ($scope, $http) {
+    var MainController = function ($scope, $http, $interval, $log) {
 
         var onUserComplete = function (response) {
             $scope.user = response.data;
@@ -19,16 +19,39 @@
             $scope.error = "can't find data";
         };
 
+        
         $scope.search = function (username) {
+            $log.info("serarching for " + $scope.username);
             $http.get("http://api.github.com/users/" + username)
                  .then(onUserComplete, onError);
+            if (intervalcountdown) {
+                $interval.cancel(intervalcountdown);
+                $scope.countdown = null;
+            }
+           
         }
 
+        var decrementcountdown = function () {
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+        }
+
+        
+       
+        var startcountdown = function () {
+            intervalcountdown=$interval(decrementcountdown, 1000, $scope.countdown);
+        }
+        
+        var intervalcountdown = null;
         $scope.username = "Angular";
         $scope.repoSortOrder = '-stargazers_count';
+        $scope.countdown = 5;
+        startcountdown();
     };
 
-    app.controller("MainController", MainController);
+    app.controller("MainController",MainController);
 
     }()); 
 
