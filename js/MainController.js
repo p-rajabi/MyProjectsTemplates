1d36430c34f27c5f274d815411b88a47a320b383
P Rajabimehr <p.rajabi.m@gmail.com>
2016-10-17 16:17:49 -0700

    seperate all controllers and put them in different files

---
+++ js/MainController.js
@@ -0,0 +1,43 @@
+﻿
+(function () {
+
+    var app = angular.module("githubView");
+
+    var MainController = function ($scope, $interval, $log, $location) {
+
+         var decrementcountdown = function () {
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+         }
+         var intervalcountdown = null;
+           var startcountdown = function () {
+            intervalcountdown=$interval(decrementcountdown, 1000, $scope.countdown);
+           }
+
+        $scope.search = function (username) {
+            $log.info("searching for " + username);
+          
+            if (intervalcountdown) {
+                $interval.cancel(intervalcountdown);
+                $scope.countdown = null;
+            };
+            $location.path("/user/" + username);
+        };
+       
+         
+        
+        
+       
+        $scope.username = "Angular";
+       
+        $scope.countdown = 5;
+        startcountdown();
+    };
+
+    app.controller("MainController",MainController);
+
+    }()); 
+
+
---
+++ js/app.js
@@ -0,0 +1,17 @@
+﻿(function () {
+ 
+    var app = angular.module("githubView", ["ngRoute"]);
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when("/main", {
+                templateUrl: "main.html",
+                controller: "MainController"
+            })
+            .when("/user/:username", {
+                templateUrl: "user.html",
+                controller:"userController"
+            })
+            .otherwise({ redirectTo: "/main" });
+    })
+
+}());
